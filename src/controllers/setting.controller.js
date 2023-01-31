ef970f46cc8bfdb102eda79dfd1c88182e83997d
anishshrestha <shresthaanish986@gmail.com>
2023-01-31 14:59:02 +0545

    controllers cleaned

--- src/controllers/setting.controller.js
+++ src/controllers/setting.controller.js
@@ -3,10 +3,6 @@ const catchAsync = require("@utils/catchAsync");
 const ApiError = require("@utils/ApiError");
 const { settingService } = require("@services");
 const pick = require("../utils/pick");
-const createSetting = catchAsync(async (req, res) => {
-  const setting = await settingService.createSetting(req.body);
-  res.status(httpStatus.CREATED).send({ success: true, setting });
-});
 
 const getSettingById = catchAsync(async (req, res) => {
   const setting = await settingService.getSettingById(req.params.settingid);
@@ -16,14 +12,6 @@ const getSettingById = catchAsync(async (req, res) => {
   res.send(setting);
 });
 
-const getSettings = catchAsync(async (req, res) => {
-  let filter = {};
-
-  let options = pick(req.query, ["limit", "page", "sort"]);
-
-  const result = await settingService.querySettings(filter, options);
-  res.send(result);
-});
 const getUserSettings = catchAsync(async (req, res) => {
   let filter = {};
   let options = pick(req.query, ["limit", "page", "sort"]);
@@ -77,27 +65,10 @@ const updateSetting = catchAsync(async (req, res) => {
   await settingService.updateSettingById(settingid, data);
   res.send(setting);
 });
-const deleteSetting = catchAsync(async (req, res) => {
-  const settingid = req.params;
-  let setting = await settingService.getSettingById(settingid);
-
-  if (!setting) {
-    console.log({
-      setting,
-    });
-    throw new ApiError(httpStatus.NOT_FOUND, "Setting not found");
-  }
-  await settingService.deleteSettingById(settingid);
-
-  res.send({ success: true, message: "Deleted successfully!" });
-});
 
 module.exports = {
-  createSetting,
   getSettingById,
-  getSettings,
   getUserSettings,
   updateSetting,
-  deleteSetting,
   createOrUpdateSetting,
 };
