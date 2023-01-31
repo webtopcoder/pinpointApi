732c0b7ead009517ccfaeb202a47009b815a2b8d
anishshrestha <shresthaanish986@gmail.com>
2023-02-01 00:15:56 +0545

    fixed shoutout in profile controller

--- src/controllers/shoutout.controller.js
+++ src/controllers/shoutout.controller.js
@@ -3,21 +3,6 @@ const catchAsync = require("@utils/catchAsync");
 const ApiError = require("@utils/ApiError");
 const { shoutoutService, userService } = require("@services");
 const pick = require("../utils/pick");
-const createShoutout = catchAsync(async (req, res) => {
-  const to_user = await userService.getUserByUsername(req.params.to_username);
-  if (!to_user) {
-    throw new ApiError(httpStatus.NOT_FOUND, "User not found");
-  } else {
-    const to_userid = to_user._id;
-    const data = {
-      from: req.user._id,
-      to: to_userid,
-      content: req.body.content,
-    };
-    const shoutout = await shoutoutService.createShoutout(data);
-    res.status(httpStatus.CREATED).send({ success: true, shoutout });
-  }
-});
 
 const getShoutoutsByUserId = catchAsync(async (req, res) => {
   let filter = pick(req.query, ["q"]);
@@ -76,7 +61,6 @@ const deleteShoutout = catchAsync(async (req, res) => {
 });
 
 module.exports = {
-  createShoutout,
   getShoutoutsByUserId,
   deleteShoutout,
 };
