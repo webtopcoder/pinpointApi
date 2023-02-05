7e5037128ef878c196a9199ed2fdabba0b0e5cb8
h3cksamrat (mint) <53104092+h3cksamrat@users.noreply.github.com>
2023-02-05 21:23:45 +0545

    style: added line spacing

--- src/routes/partnership.route.js
+++ src/routes/partnership.route.js
@@ -5,6 +5,7 @@ const { partnershipController } = require("@controllers");
 const router = express.Router();
 
 router.route("/").get(auth(), partnershipController.getPartnerships);
+
 router
   .route("/create-customer")
   .post(auth(), partnershipController.createCustomer);
@@ -12,12 +13,15 @@ router
 router
   .route("/subscribe")
   .post(auth(), partnershipController.subscribePartnership);
+
 router
   .route("/cancel-subscribe")
   .delete(auth(), partnershipController.cancelSubscription);
+
 router
   .route("/create-transaction")
   .post(auth(), partnershipController.createTransaction);
+
 router.route("/:id").get(auth(), partnershipController.getPartnershipById);
 
 module.exports = router;
