94b820b781ef727925f961c8193c835fe8f06aa3
webtopcoder <rkvirtys109@gmail.com>
2023-04-27 05:13:02 +0200

    following list issue

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -7,6 +7,8 @@ const { Follow } = require("../models");
 
 const getFollowers = catchAsync(async (req, res) => {
   const { userId } = pick(req.params, ["userId"]);
+
+  console.log(userId)
   let filter = pick(req.query, ["q"]);
 
   let options = pick(req.query, ["limit", "page", "sort"]);
