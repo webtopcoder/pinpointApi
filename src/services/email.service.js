53a6632e5b89128eb54af651eada6ac30cad3497
webtopcoder <rkvirtys109@gmail.com>
2023-05-26 17:31:04 +0200

    fixed: additional user template added, some issue

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -98,14 +98,6 @@ const sendVerificationEmail = async (userId) => {
 
 const sendInviteEmail = async ({ senderId, inviteTo, message }) => {
   const user = await userService.getUserById(senderId);
-  // const link = `${config.frontend_url}/home`;
-  // const defaultMessage = `${message ? message : ""
-  //   }<br><p>Hi, You have been invited to join The Pinpoint Social by ${user.firstName + " " + user.lastName
-  //   }.</p><p>Please click on the following <a href="${link}">link</a> to verify your email.</p>`;
-  // const subject = "Invitation";
-  // const to = inviteTo;
-  // const html = defaultMessage;
-
   await sendEmailWithEJS(inviteTo, subject, message ? "invite-email-message" : 'invite-email', {
     title: "Invitation",
     message: message,
@@ -117,36 +109,17 @@ const sendInviteEmail = async ({ senderId, inviteTo, message }) => {
 
 const sendPartnerStatus = async ({ id }) => {
   const user = await userService.getUserById(id);
-  const link = `${config.frontend_url}/`;
-  // const activeMessage = `<p>Your Approved!</p><p>You can now access The Pinpoint Soical. Thank you for your for your patience.</p><p><a href="${link}">Login</a></p>
-  // <p>You must have a pinpoint Partnership to go live on our interactive map.</p>
-  // <p>If you did not intend to receive this email, please ignore this email.</p>`;
-  // const inactiveMessage = `<p>Access Denied</p><p>For one reason or another, Pinpoint has declined your request to access the platform.</p>
-  // <p>If you belive this may have been a mistake, please reach out to us at.. pinpointfoodtruck@gmail.com</p>
-  // <p>If you did not intend to receive this email, please ignore this email.</p>`;
-  const subject = "Partner Verication";
-  // const to = user.email;
-  // const html = user.status === "active" ? activeMessage : inactiveMessage;
-  // await sendEmail(to, subject, html);
-
+  const subject = "Partner Verification";
   await sendEmailWithEJS(user.email, subject, user.status ? "partner-approve" : 'partner-decline', {
     title: "Partner Verication",
   });
-
-
 };
 
 const sendAdditionUserEmail = async ({ owner_id, additional }) => {
   const user = await userService.getUserById(owner_id);
   const token = await tokenService.generateCreateAdditionToken(user);
   const link = `https://testing.thepinpointsocial.com/authentication/additionuser/register/?token=${token}&&partner=${user.email}&&user=${additional.email}&&partnerID=${owner_id}`;
-  // const defaultMessage = `<p>Hi, ${user?.email} added you as ${additional?.role}.</p><p>Please click on the following <a href="${link}">link</a> to verify your email.</p>
-  // <br><p>If you did not request this, please ignore this email.</p>`;
   const subject = "Invitation As additional user";
-  // const to = additional?.email;
-  // const html = defaultMessage;
-  // await sendEmail(to, subject, html);
-
   await sendEmailWithEJS(additional.email, subject, "additional-user-invite", {
     title: "Additional User Invitation",
     link: link
